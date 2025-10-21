b27f81a01ed6c452d03257024b0993d794b2974e
akarani-cs <alvinmudembei@gmail.com>
2025-10-21 17:02:47 +0300

    reverse

--- FRONTEND/airbnb-clone/src/App.jsx
+++ FRONTEND/airbnb-clone/src/App.jsx
@@ -2,25 +2,19 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Details from './pages/Details'
 import Checkout from './pages/Checkout'
 
 function App() {
   return (
-
-
     <BrowserRouter>
-
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details" element={<Details />} />
         <Route path="/checkout" element={<Checkout />} />
-
       </Routes>
     </BrowserRouter>
-
-    
   )
 }
 
---
+++ FRONTEND/airbnb-clone/src/pages/Details.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Details() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Details</h1>
+      <p className="text-gray-600">Details page placeholder.</p>
+    </main>
+  )
+}
