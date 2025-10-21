99779e7616ca1875ff9ebec19288503861511d15
akarani-cs <alvinmudembei@gmail.com>
2025-10-21 17:29:34 +0300

    card

---
+++ FRONTEND/airbnb-clone/src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Cards() {
+    return (
+       
+       <div className="bg-white rounded-lg shadow-md overflow-hidden">
+      {/* Image */}
+      <div className="h-60 bg-gray-200">
+        {/* We'll add the image here next */}
+      </div>
+
+      {/* Content */}
+      <div className="p-4">
+        {/* We'll add location, price, rating, and amenities here */}
+      </div>
+    </div>
+  );
+};
+
+    
